0aac8c70bb05d9bf05dcdfe8c3941749a1f7e8db
Lukasz Cena <lbjcena@gmail.com>
2023-01-25 15:34:34 +0100

    multiple click protection added

--- js/script.js
+++ js/script.js
@@ -15,43 +15,47 @@ console.log(cellHeight, cellWidth)
 
 
 gameCell.forEach((e) => {
-    e.addEventListener('click', () => {
-        
 
+    // e.marked method is used to check if the clicked square is already filled - when filled, turns to true and prevents filling a square with multiple Xs or Os
+    e.marked = false
+    e.addEventListener('click', () => {
         const drawX = () => {
-    
-            let context = e.getContext('2d');
-            context.moveTo(10, 10);
-            context.lineTo(140, 140);
-            context.stroke();
-            context.moveTo(120, 10);
-            context.lineTo(10, 140);
-            context.stroke();
-            console.log('works');
+            if(e.marked === false){
+                e.marked = true
+                let context = e.getContext('2d');
+                context.moveTo(10, 10);
+                context.lineTo(140, 140);
+                context.stroke();
+                context.moveTo(120, 10);
+                context.lineTo(10, 140);
+                context.stroke();
+                console.log('works');
+                
+            }
         }
 
         const drawCircle = () => {
-            
-            let context = e.getContext('2d');
-            context.beginPath(0, 0);
-            context.arc(cellHeight/3.5, cellWidth/3.5, 40, 0, 2*Math.PI )
-            context.stroke()
+            if(e.marked === false){
+                e.marked = true
+                let context = e.getContext('2d');
+                context.beginPath(0, 0);
+                context.arc(cellHeight/3.5, cellWidth/3.5, 40, 0, 2*Math.PI )
+                context.stroke()
+            }
         }
         
         let play = () => {
             currentlyPlaying.innerHTML = ""
-            if(playing % 2 === 0){
+            if(playing % 2 === 0 && e.marked === false){
                 drawX()
                 playing += 1 
                 currentlyPlaying.innerHTML = 'Now playing : O'
-            } else if (playing % 2 === 1) {
+            } else if (playing % 2 === 1 && e.marked === false) {
                 drawCircle()
                 playing += 1
                 currentlyPlaying.innerHTML = 'Now playing : X'
             }
         }
-
-        
         play()
     })
 })
