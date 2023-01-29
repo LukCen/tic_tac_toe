983fa6c760d42cb085bcca721871aa227763e3e3
Lukasz Cena <lbjcena@gmail.com>
2023-01-29 20:48:45 +0100

    reset button added

--- js/script.js
+++ js/script.js
@@ -1,11 +1,14 @@
 'use strict'
 
 const cells = document.querySelectorAll('.gameCell');
+const container = document.querySelector('.buttonContainer')
 let playing = 2;
 
 cells.forEach((c)=> {
 
     c.addEventListener('click', () => {
+       
+        // dynamically create both X and O
         const drawCross = () => {
             let crossLeft = document.createElement('div');
             let crossRight = document.createElement('div');
@@ -24,7 +27,7 @@ cells.forEach((c)=> {
         }
 
         const play = () => {
-            
+            // draws a correct symbol in the clicked cell - always starts with X
             if(c.classList.contains('empty')){
                 (playing % 2 === 0) ? drawCross() : drawCircle();
                 c.classList.remove('empty')
@@ -33,9 +36,27 @@ cells.forEach((c)=> {
                 console.log(playing)
             }
 
+            if(playing === 11){
+                
+                // create the reset button, give it "button" type and add a styling class
+                let reset = document.createElement('button');
+                reset.type = 'button';
+                reset.classList.add('restart');
+                reset.innerHTML = 'Clear'
+                container.appendChild(reset)
+
+                // resets the board upon clicking then deletes itself
+                reset.addEventListener('click', () => {
+                    cells.forEach((c) => {
+                        c.classList.add('empty')
+                        c.innerHTML = ''
+                        playing = 2;
+                        
+                    })
+                    container.removeChild(reset)
+                })
+            }
         }
-        
-        
         play()
     })
     
